c762d6075d764be3b5b965b04ec60dd741ba3072
Mu Yang <emfomy@gmail.com>
2021-01-26 16:13:37 +0800

    Update about section.

--- src/components/sections/about.vue
+++ src/components/sections/about.vue
@@ -14,44 +14,10 @@
     </template>
 
     <p>
-      I currently work as a research assistant at the
-      <b-link href="https://www.iis.sinica.edu.tw/en/index.html" target="_blank"
-        >Institute of Information Science, Academia Sinica</b-link
-      >, advised by
-      <b-link href="https://www.iis.sinica.edu.tw/pages/ma/" target="_blank"
-        >Dr. Wei-Yun Ma</b-link
-      >
-      at the
-      <b-link href="https://ckip.iis.sinica.edu.tw" target="_blank"
-        >Chinese Knowledge and Information Processing (CKIP) Lab</b-link
-      >, conducting researches of natural language processing and computational
-      linguistics.
-    </p>
-
-    <p>
-      Previously, I received my master degree at
-      <b-link href="http://www.math.ntu.edu.tw/~iams/eng" target="_blank"
-        >Institute of Applied Mathematical Sciences, National Taiwan
-        University</b-link
-      >, advised by
-      <b-link href="https://wwang.info/" target="_blank"
-        >Dr. Weichung Wang</b-link
-      >, conducting researches of high-performance parallel computing on hybrid
-      CPU-GPU structures.
-    </p>
-
-    <p>
-      I have working experience in
-      <b-link href="https://www.research.ibm.com/labs/watson/" target="_blank"
-        >Thomas J. Watson Research Center, IBM Corporation</b-link
-      >
-      as an internship, advised by
-      <b-link
-        href="https://researcher.watson.ibm.com/researcher/view.php?person=us-ihchung"
-        target="_blank"
-        >Dr. I-Hsin Chung</b-link
-      >, conducting researches of high-performance parallel computing on hybrid
-      CPU-GPU structures.
+      A data engineer and full‐stack developer with a research background in
+      both computer science and applied mathematics domains. Skilled in
+      front-end web design, deep learning, and parallel computing researches.
+      Experienced in computer cluster formation and maintenance.
     </p>
 
     <IconButton
